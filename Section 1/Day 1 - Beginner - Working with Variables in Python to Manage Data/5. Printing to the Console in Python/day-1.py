91a16cf3b452dd3a1ef5ff0e894d07f865e4490b
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-08-14 18:28:02 +0700

    Initial commit

---
+++ Section 1/Day 1 - Beginner - Working with Variables in Python to Manage Data/5. Printing to the Console in Python/day-1.py
@@ -0,0 +1,3 @@
+# to print string
+
+print("Hello World!")
---
+++ Section 1/Day 1 - Beginner - Working with Variables in Python to Manage Data/6. [Interactive Coding Exercise] Printing/day-1-1-test-your-code.py
@@ -0,0 +1,12 @@
+# output should like this
+
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+
+# answer
+print('Day 1 - Python Print Function')
+
+print('The function is declared like this')
+
+print("print('what to print')")
