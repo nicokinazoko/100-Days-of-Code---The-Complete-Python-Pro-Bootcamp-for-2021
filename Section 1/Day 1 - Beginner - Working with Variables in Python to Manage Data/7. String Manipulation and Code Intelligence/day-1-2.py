ac3ac20e163cf8acf3e307f693f8b138c2ad968e
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-08-14 19:27:58 +0700

    Create day-1-2.py

---
+++ Section 1/Day 1 - Beginner - Working with Variables in Python to Manage Data/7. String Manipulation and Code Intelligence/day-1-2.py
@@ -0,0 +1,23 @@
+# print('Hello World!')
+# print('Hello World!')
+# print('Hello World!')
+
+# this is not efficient
+
+# how to make it efficient?
+# using \n for break
+
+# example
+print("Hello World!\nHello World!")
+
+# but can it run with another slash?
+
+# answer
+print("Hello World!\nHello World!\nHello World!")
+
+# combining string
+# using +
+# can add space in print with + 
+# or using string concatenation : ' '
+
+print("Hello " + "Nico")
