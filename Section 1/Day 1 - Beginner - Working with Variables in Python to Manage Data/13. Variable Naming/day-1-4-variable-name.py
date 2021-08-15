45d78c5f4ae89dba241ea2108b392e11ab24dec7
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-08-15 18:08:09 +0700

    Create day-1-4-variable-name.py

---
+++ Section 1/Day 1 - Beginner - Working with Variables in Python to Manage Data/13. Variable Naming/day-1-4-variable-name.py
@@ -0,0 +1,25 @@
+# using variable to store data
+
+# example
+
+# save data using variable 'name'
+# name = input("What is your name?")
+name = "Nico"
+
+print(name)
+
+# can assign variable to new value
+name = "Nico Nico"
+print(name)
+
+
+
+
+user_name = input('Your Name = ')
+length = len(user_name)
+print(length)
+
+# some of naming variable rules
+# 1. use underscore _ to separate words
+# 2. not using number first letter of variable
+# 3. dont use same name as function like print, input, etc
