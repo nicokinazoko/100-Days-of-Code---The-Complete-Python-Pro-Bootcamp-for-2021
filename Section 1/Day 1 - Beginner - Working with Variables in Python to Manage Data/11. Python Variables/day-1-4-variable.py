115d64b4da53819649bbd453060020363a9db76c
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-08-15 18:08:03 +0700

    Create day-1-4-variable.py

---
+++ Section 1/Day 1 - Beginner - Working with Variables in Python to Manage Data/11. Python Variables/day-1-4-variable.py
@@ -0,0 +1,20 @@
+# using variable to store data
+
+# example
+
+# save data using variable 'name'
+# name = input("What is your name?")
+name = "Nico"
+
+print(name)
+
+# can assign variable to new value
+name = "Nico Nico"
+print(name)
+
+
+
+
+name = input('Your Name = ')
+length = len(name)
+print(length)
