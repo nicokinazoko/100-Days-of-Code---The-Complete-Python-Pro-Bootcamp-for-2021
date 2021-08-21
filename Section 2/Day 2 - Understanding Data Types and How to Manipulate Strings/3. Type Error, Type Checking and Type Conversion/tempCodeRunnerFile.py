a4dac8460145132c68e17ef4afa85f1ac635e090
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-08-21 18:50:29 +0700

    Create tempCodeRunnerFile.py

---
+++ Section 2/Day 2 - Understanding Data Types and How to Manipulate Strings/3. Type Error, Type Checking and Type Conversion/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+num_char = len(input("What is your Name? "))
+
+# # to convert data types
+# new_num_char = str(num_char)
+# # print("Your name has " + num_char + " characters")
+# # error because can only concatenate string, not int
+
+# # after conversion
+# print("Your name has " + new_num_char + " characters")
+# # to know the data types
+# print(type(num_char))
