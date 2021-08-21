6ee97603be4e91ff0772242fe81120461ea5302c
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-08-21 18:50:25 +0700

    Create day-2-1-primitive-data-types.py

---
+++ Section 2/Day 2 - Understanding Data Types and How to Manipulate Strings/2. Python Primitive Data Types/day-2-1-primitive-data-types.py
@@ -0,0 +1,18 @@
+#data Types
+# 1. String
+
+# to output first letter of the string
+# called subscript
+print("Hello"[4])
+
+# 2. Integers
+# without decimal
+
+print(123)
+print(123+456)
+# 3. Float 
+# number with decimal
+print(22/7)
+
+# 4. Boolean
+# true or false
