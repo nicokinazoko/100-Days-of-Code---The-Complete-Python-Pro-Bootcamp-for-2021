f922278144e7a47d8c7fe2e2b178b3dca4f20486
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-08-21 18:50:31 +0700

    Create day-2-1-exercise.py

---
+++ Section 2/Day 2 - Understanding Data Types and How to Manipulate Strings/4. [Interactive Coding Exercise] Data Types/day-2-1-exercise.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+number_1 = int(two_digit_number[0])
+number_2 = int(two_digit_number[1])
+print(number_1 + number_2)
+
+
+
+
+
+
+
+
+
