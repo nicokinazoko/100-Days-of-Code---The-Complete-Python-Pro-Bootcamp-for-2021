e0b7cbb6841541ad118a839f5cf91760d380fa53
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-08-21 18:50:27 +0700

    Create day-2-2.py

---
+++ Section 2/Day 2 - Understanding Data Types and How to Manipulate Strings/3. Type Error, Type Checking and Type Conversion/day-2-2.py
@@ -0,0 +1,17 @@
+# num_char = len(input("What is your Name? "))
+
+# # to convert data types
+# new_num_char = str(num_char)
+# # print("Your name has " + num_char + " characters")
+# # error because can only concatenate string, not int
+
+# # after conversion
+# print("Your name has " + new_num_char + " characters")
+# # to know the data types
+# print(type(num_char))
+
+a = float(123)
+print(type(a))
+
+print(70 + float("100.5"))
+print(str(70) + str(100))
