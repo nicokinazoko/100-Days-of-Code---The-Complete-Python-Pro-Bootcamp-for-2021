2d8400d0aa8656314db1414b0df629285b7d0a9c
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-08-28 19:26:36 +0700

    Create day-2-3-Number-population.py

---
+++ Section 2/Day 2 - Understanding Data Types and How to Manipulate Strings/7. Number Manipulation and F Strings in Python/day-2-3-Number-population.py
@@ -0,0 +1,37 @@
+print(8/3)
+
+
+# to round number
+# use round
+# to use precision
+
+# meaning 2 numbers behind coma
+print(round(8/3, 2))
+
+# to convert divide into int without convert
+# using // 
+print(8//3)
+
+
+# to use divide twice
+# using /=
+
+result = 4/2
+result /= 2
+print(result)
+
+# to add +=
+# to minus -=
+# to multiply *=
+# to divide /=
+
+
+score = 0
+score += 1
+height = 1.8
+isWinning = True
+
+# f string
+# using f behind string
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
+print('your score is ' + str(score))
