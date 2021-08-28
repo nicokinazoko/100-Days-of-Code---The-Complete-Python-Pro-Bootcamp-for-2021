26bd3868fc686698494477c94338a7768e6839f1
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-08-28 19:26:44 +0700

    Create day-2-3-exercise.py

---
+++ Section 2/Day 2 - Understanding Data Types and How to Manipulate Strings/8. [Interactive Coding Exercise] Life in Weeks/day-2-3-exercise.py
@@ -0,0 +1,23 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?\n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+ageTotal = 90
+ageNumber = int(age)
+
+totalDays = (90 - ageNumber) * 365
+totalWeeks = (90 - ageNumber) * 52
+totalMonths = (90 - ageNumber) * 12
+
+print(f"You have {totalDays} days, {totalWeeks} weeks, and {totalMonths} months left")
+
+
+
+
+
+
+
+
+
